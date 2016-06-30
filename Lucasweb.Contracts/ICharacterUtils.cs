3de5c57ac25cb21ad649eacc9013a0bdadae1d9d
Hermanoid <lucasniewohner@gmail.com>
2016-06-30 12:55:04 -0500

    Password Encryption is now complete.

---
+++ Lucasweb.Contracts/ICharacterUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucasweb.Contracts
+{
+    public interface ICharacterUtils
+    {
+        int addAsciiValsOf(string message);
+    }
+}
---
+++ Lucasweb/Models/Password.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucasweb.Models
+{
+    public class Password
+    {
+
+        [Required]
+        public string password { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string message { get; set; }
+
+        [Required]
+        [Display(Description = "Encrypt")]
+        public bool isEncrypt { get; set; }
+    }
+}
