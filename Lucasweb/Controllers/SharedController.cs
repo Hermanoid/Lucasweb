e72253f7cea913ce201065e52da3674e9e4661ef
Hermanoid <lucasniewohner@gmail.com>
2016-06-30 12:59:46 -0500

    DropDowns, All Projects and About pages, updated GitIgnore, and generally stuff that should've been commited to master or a different branch.

---
+++ Lucasweb/Controllers/SharedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Lucasweb.Controllers
+{
+    public class SharedController : Controller
+    {
+
+        [ActionName("_DropDownStuff")]
+        public PartialViewResult _DropDownStuff()
+        {
+            ViewData["DropDownStuff"] = "Drop down worked to.";
+            return PartialView();
+        }
+    }
+}
